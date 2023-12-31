e4187cd919695fbe457620c4892f580ef120d657
ulugbekpy <ulugbekpycpp@gmail.com>
2023-12-31 17:02:22 +0500

    register, login templates but not completed

--- link.py
+++ link.py
@@ -24,7 +24,9 @@ def register():
         return redirect(url_for("home"))
     return render_template("register.html",title="Register",form=form)
 
-@app.route("/login")
+@app.route("/login",methods=['GET','POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        return redirect(url_for("home"))
     return render_template("login.html",title="Login",form=form)
